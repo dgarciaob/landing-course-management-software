d9789051372b5d57f7fa1555bf319cef90b8ebdc
Diego <diegogaroban@hotmail.com>
2024-04-06 21:11:04 -0500

    add: courses table

--- app/(admin)/teacher/courses/page.tsx
+++ app/(admin)/teacher/courses/page.tsx
@@ -1,12 +1,28 @@
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const courses = await db.course.findMany({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>Nuevo Curso</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
